fe4d37deb214f59ce64baf2cb32f88884ffeacb5
Cedric David <cedric.david@btopenworld.com>
2018-09-05 01:07:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
   attr_accessor :name
-  @@songs_count = 0
+  @@song_count = 0
   def initialize(name)
     @name = name
     @songs = []
@@ -11,16 +11,16 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @@songs_count +=1
+    @@song_count +=1
   end
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @@songs_count +=1
+    @@song_count +=1
   end
   def self.song_count
-    
+
       @@songs_count
     
   end
