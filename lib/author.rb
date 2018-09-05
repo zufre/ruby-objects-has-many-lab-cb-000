267871010cab62cd43d361da19e9dffcbba3f73a
Cedric David <cedric.david@btopenworld.com>
2018-09-05 01:29:36 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+  @@post_count = 0 
   def initialize(name)
       @name = name
       @posts = []
@@ -10,11 +11,12 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count ++ 1
   end
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
     @posts << post
-
+    @@post_count += 1
   end
 end
