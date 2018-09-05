50770090e55a69ee87073be1c73825c0590080f0
Cedric David <cedric.david@btopenworld.com>
2018-09-05 00:58:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,11 @@ class Artist
     @songs
   end
   def add_song(song)
-    
+    @songs << song
+    song.artist = self
+  end
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
   end
