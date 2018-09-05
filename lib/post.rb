279710e6a2b288169ef54db595c7ad78720e090f
Cedric David <cedric.david@btopenworld.com>
2018-09-05 00:42:38 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,20 +1,4 @@
 class Post
-  attr_accessor :author, :title, :posts
-
-  def initialize(title)
-    @title = title
-  end
-
-  def author_name
-    if self.author != nil
-      self.author.name
-    else
-      nil
-    end
-  end
-
-  def title
-    @title
-  end
+  
 
 end
